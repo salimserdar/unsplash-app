c7bf41c8958d5db91dc090667e2ddba6612482a7
Salim Serdar KOKSAL <salimserdar@gmail.com>
2021-05-19 03:34:01 -0700

    minor update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from 'components/Layout'
-import Gallery from 'components/Gallery'
-import Stats from 'components/Stats'
 import Photos from 'components/Photos'
 
 const Home = () => {
